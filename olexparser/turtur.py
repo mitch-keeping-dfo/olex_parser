b466f605c41ab17bab3f4016d6f3b9a83b893b26
mitchell.keeping <mitch.keeping.dfo@gmail.com>
2022-01-13 16:06:55 -0330

    added check min/max;

--- olexparser/turtur.py
+++ olexparser/turtur.py
@@ -29,8 +29,9 @@ class TurTur:
 
         :param tur_num: the Tur Tur number as listed in the Turdata file
         :type tur_num: int
-        :param tur_segment_summaries: A list of TurTurSegmentSummaries.
-        :type tur_segment_summaries: list
+        :param tur_segment_summaries: A dict of TurTurSegmentSummaries with
+                                      key:value == Segment Number:TurTurSegmentSummary.
+        :type tur_segment_summaries: dict
         """
 
         self.tur_num = tur_num
@@ -42,6 +43,120 @@ class TurTur:
         # self.parse_segment_files()
         # self.check_sample_sizes()
         return
+
+    def check_min_max_all(self):
+        """
+        Checks the min and max values for the latitude, longitude, and time in all the :class:`TurTurSegmentSummary`
+        objects against their :class:`SegmentFile`.
+
+        :return: True if all min/max values in Segments match those given in their summary; False otherwise
+        :rtype: bool
+        """
+        same = True
+        for seg_num in self.segments_summaries.keys():
+            if not self.check_min_max(seg_num):
+                # warnings handled in self.check_min_max()
+                same = False
+        return same
+
+    def check_min_max(self, seg_num):
+        """
+        .. todo:: check that min/max values in summary match min/max in segment file
+
+        Checks the min and max values for the latitude, longitude, and time in the :class:`TurTurSegmentSummary`
+        against the :class:`SegmentFile`.
+
+        :return: True if all min/max values in Segment match those given in the summary; False otherwise
+        :rtype: bool
+        """
+        same = True
+
+        # check if the segment file is associated to the TurTur
+        if seg_num in self.segments.keys():
+
+            # get the TurTurSegmentSummary
+            seg_sum = self.segments_summaries[seg_num]
+
+            # get the values from the TurTurSegmentSummary
+            sum_lat_small = seg_sum.get_lat_small_float()
+            sum_long_small = seg_sum.get_long_small_float()
+            sum_lat_large = seg_sum.get_lat_large_float()
+            sum_long_large = seg_sum.get_long_large_float()
+            sum_time_start = seg_sum.get_time_start_int()
+            sum_time_end = seg_sum.get_time_end_int()
+
+            seg_entries = self.segments[seg_num].get_seg_entries()
+
+            # convert to list so we can use subscripting
+            values_list = list(seg_entries.values())
+
+            # set initial values
+            first_value = values_list[0]
+            largest_lat = first_value.get_lat_float()
+            smallest_lat = first_value.get_lat_float()
+            largest_long = first_value.get_long_float()
+            smallest_long = first_value.get_long_float()
+            largest_time = first_value.get_timestamp_int()
+            smallest_time = first_value.get_timestamp_int()
+
+            # get min/max values in segment
+            for entry in values_list[1:]:
+                if entry.get_lat_float() > largest_lat:
+                    largest_lat = entry.get_lat_float()
+                if entry.get_lat_float() < smallest_lat:
+                    smallest_lat = entry.get_lat_float()
+                if entry.get_long_float() > largest_long:
+                    largest_long = entry.get_long_float()
+                if entry.get_long_float() < smallest_long:
+                    smallest_long = entry.get_long_float()
+                if entry.get_timestamp_int() > largest_time:
+                    largest_time = entry.get_timestamp_int()
+                if entry.get_timestamp_int < smallest_time:
+                    smallest_time = entry.get_timestamp_int()
+
+            if smallest_lat != sum_lat_small:
+                warn = "Warning, TurTur {} Segment {}: the summary has the smallest latitude value as {}, the " \
+                       "actual smallest value is {}".format(self.tur_num, seg_num, sum_lat_small, smallest_lat)
+                self.warnings.append(warn)
+                same = False
+
+            if smallest_long != sum_long_small:
+                warn = "Warning, TurTur {} Segment {}: the summary has the smallest longitude value as {}, the " \
+                       "actual smallest value is {}".format(self.tur_num, seg_num, sum_long_small, smallest_long)
+                self.warnings.append(warn)
+                same = False
+
+            if largest_lat != sum_lat_large:
+                warn = "Warning, TurTur {} Segment {}: the summary has the largest latitude value as {}, the " \
+                       "actual largest value is {}".format(self.tur_num, seg_num, sum_lat_large, largest_lat)
+                self.warnings.append(warn)
+                same = False
+
+            if largest_long != sum_long_large:
+                warn = "Warning, TurTur {} Segment {}: the summary has the largest longitude value as {}, the " \
+                       "actual longitude value is {}".format(self.tur_num, seg_num, sum_long_large, largest_long)
+                self.warnings.append(warn)
+                same = False
+
+            if largest_time != sum_time_end:
+                warn = "Warning, TurTur {} Segment {}: the summary has the largest time value as {}, the " \
+                       "actual largest time is {}".format(self.tur_num, seg_num, sum_time_end, largest_time)
+                self.warnings.append(warn)
+                same = False
+
+            if smallest_time != sum_time_start:
+                warn = "Warning, TurTur {} Segment {}: the summary has the smallest time value as {}, the " \
+                       "actual smallest time is {}".format(self.tur_num, seg_num, sum_time_start, smallest_time)
+                self.warnings.append(warn)
+                same = False
+
+        else:
+            warn = "Warning, Segment {} not associated with TurTur {}. Either the file does not exist or it wasn't " \
+                   "added to the TurTur object.".format(seg_num, self.tur_num)
+            self.warnings.append(warn)
+            same = False
+
+        return same
 
     def add_segment(self, seg_num, segment):
         """
@@ -64,19 +179,16 @@ class TurTur:
 
     def check_sample_sizes(self):
         """
-        A method for checking if given size of the segment file in summary is the same as the actual size on disk.
+        A method for checking if given size of the segment files in summaries is the same as the actual size on disk.
 
         Adds a warning if different.
         """
-        for summary in self.segments_summaries:
-            seg_num = summary.get_seg_num()
-            segment = self.segments[seg_num]
-            expected_size = summary.get_entries_num() * 16
-            actual_size = segment.get_size()
-            if segment.get_size() != expected_size:
-                warn = "Warning, Tur Tur expects Segment {} to have file size {}, actual size is {}".format(
-                    seg_num, expected_size, actual_size)
+        for seg_num in self.segments_summaries.keys():
+            if not self.check_sample_size(seg_num):
+                warn = "Warning, expected size of Segment {} differs from actual size".format(seg_num)
+
                 self.warnings.append(warn)
+
         return
 
     def get_segment_numbers(self):
